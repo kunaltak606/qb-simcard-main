5c470398d4d4309c555166e14e55e5a7945ca40b
kunaltak606 <74263158+kunaltak606@users.noreply.github.com>
2021-03-24 11:03:45 +0530

    first

---
+++ README.md
@@ -0,0 +1,25 @@
+# QBCore-simcard
+
+# credits
+- Scorpion#1806 - https://github.com/kunaltak606
+- https://discord.gg/DgcN4y8
+
+This resource for Qbus adds a cellphone simcard item, and allows for players to change their cellphone number
+
+
+
+## Installation
+- Clone or download this repository
+- Add qb-simcard to your resources directory
+- Add this in your server.cfg : start qb-simcard
+- Change Core Name QBCore to your Core Name
+- Add this line in your core - shared.lua
+	["sim_card"] 		 				 = {["name"] = "sim_card", 						["label"] = "Sim Card", 					["weight"] = 100, 	["type"] = "item", 		["image"] = "sim_card.png", 	["unique"] = true, 	["useable"] = true, 	["shouldClose"] = true,	   ["combinable"] = nil,   ["description"] = "Change Your phone Number to Any Number (One-time use)"},
+
+-  Add Image in your Inventory Item Inages
+
+
+
+# Done Now Use Sim Card Item to Change Phone Number
+
+
---
+++ server.lua
@@ -0,0 +1,33 @@
+QBCore = nil
+TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
+
+RegisterServerEvent('qb-simcard:useSimCard')
+AddEventHandler('qb-simcard:useSimCard', function(number)
+    local src = source
+    TriggerClientEvent('qb-simcard:startNumChange', src, number)     
+end)
+
+RegisterServerEvent('qb-simcard:changeNumber')
+AddEventHandler('qb-simcard:changeNumber', function(newNum)
+    TriggerClientEvent('qb-simcard:success', source, newNum)
+end)
+
+QBCore.Functions.CreateUseableItem("sim_card", function(source, item)
+    local src = source
+    local xPlayer = QBCore.Functions.GetPlayer(src)    
+    TriggerClientEvent('qb-simcard:changeNumber', source, xPlayer)
+end)
+
+RegisterServerEvent('qb-simcard:changeNumber')
+AddEventHandler('qb-simcard:changeNumber', function(MData)
+    local xPlayer = QBCore.Functions.GetPlayer(source)
+    QBCore.Functions.ExecuteSql(false, "SELECT * FROM `players` WHERE `citizenid` = '"..xPlayer.PlayerData.citizenid.."'", function(result)
+        local MetaData = json.decode(result[1].metadata)
+        local Charinfo = json.decode(result[1].charinfo)
+        MetaData.phone = MData
+        Charinfo.phone = MData
+        QBCore.Functions.ExecuteSql(false, "UPDATE `players` SET `metadata` = '"..json.encode(MetaData).."' WHERE `citizenid` = '"..xPlayer.PlayerData.citizenid.."'")
+        QBCore.Functions.ExecuteSql(false, "UPDATE `players` SET `charinfo` = '"..json.encode(Charinfo).."' WHERE `citizenid` = '"..xPlayer.PlayerData.citizenid.."'")
+    end)
+    xPlayer.Functions.SetMetaData("phone", MData)
+end)
